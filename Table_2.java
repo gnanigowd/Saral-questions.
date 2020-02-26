fce949433fc94d5be1193830dfb375d6302d6874
gnanigowd <54390788+gnanigowd@users.noreply.github.com>
2020-02-27 00:33:52 +0530

    Add files via upload

---
+++ Table_2.java
@@ -0,0 +1,19 @@
+package interviewQuestion;
+
+public class Table_2 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for (int i = 2; i<=20;i++) {
+			if (i%2==0) {
+				num = num+1;
+				System.out.println("2 * " + num + "  =  " + i);
+				
+				
+			}
+			
+		}
+
+	}
+
+}
